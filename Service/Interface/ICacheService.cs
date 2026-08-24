c9e6e1d2d88fe35cfbdc26696fde4a03565753ee
Pritom <psazzadul@gmail.com>
2026-08-24 19:13:37 +0600

    Add CacheService implementation and ICacheService interface for caching functionality

---
+++ Service/Interface/ICacheService.cs
@@ -0,0 +1,12 @@
+﻿namespace FlightBookingCS.Service.Interface
+{
+    public interface ICacheService
+    {
+        Task<T?> GetTAsync<T>(string key);
+        Task setAsync<T>(string key, T value, TimeSpan? expiration = null);
+        Task<bool> RemoveAsync(string key);
+        Task<bool> ExistsAsync(string key);
+        Task<string> GenerateCacheKey(string igxKey, object metadata);
+
+    }
+}
